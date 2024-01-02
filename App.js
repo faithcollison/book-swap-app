3660207a2ed9c8c0ad1a942d290d78a82c959495
Jake <jakehuntlyrobinson@gmail.com>
2024-01-02 17:56:24 +0000

    fix some button things

--- App.js
+++ App.js
@@ -1,34 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import Welcome from './components/Welcome';
-import Login from './components/Login';
-import SignUp from './components/SignUp';
+import Welcome from "./components/Welcome";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View >
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name='Welcome' component={Welcome}/>
-          <Stack.Screen name='Login' component={Login}/>
-          <Stack.Screen name='SignUp' component={SignUp}/>
-          {/* <Stack.Screen name='Home' component={Home}/> */}
-        </Stack.Navigator>
-      </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Welcome" component={Welcome}/>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+        {/* <Stack.Screen name='Home' component={Home}/> */}
+      </Stack.Navigator>
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-})
+ 
+});
--- components/Welcome.js
+++ components/Welcome.js
@@ -1,36 +1,49 @@
 import { View, Text, Pressable, StyleSheet } from "react-native";
 
-export default function Welcome ({navigation}) {
-    return (
-        <View style={styles.container}>
-            <Text>Welcome!</Text>
+export default function Welcome({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.centerText}>Welcome!</Text>
 
-            <View style={styles.buttonContainer}>
-                <Pressable onPress={() => {
-                    navigation.navigate('Login')
-                }}>
-                    <Text>Log In</Text>
-                </Pressable>
-            </View>
+      <View style={styles.buttonContainer}>
+        <Pressable
+          onPress={() => {
+            navigation.navigate("Login");
+          }}
+        >
+          <Text>Log In</Text>
+        </Pressable>
+      </View>
 
-            <View style={styles.buttonContainer}>
-                <Pressable onPress={() => {
-                    navigation.navigate('SignUp')
-                }}>
-                    <Text>Sign Up</Text>
-                </Pressable>
-            </View>
-        </View>
-    )
+      <View style={styles.buttonContainer}>
+        <Pressable
+          onPress={() => {
+            navigation.navigate("SignUp");
+          }}
+        >
+          <Text>Sign Up</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    buttonContainer: {
-        width: 320,
-        height: 68,
-        marginHorizontal: 20,
-        padding: 3,
-        alignItems: "center",
-        justifyContent: "center",
-    },
-})
+  buttonContainer: {
+    width: 100,
+    height: 68,
+    marginHorizontal: 20,
+    padding: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'blue'
+  },
+  container: {
+    // borderColor: "black",
+    // borderCurve: "12px",
+    alignItems: 'center'
+  },
+  centerText: {
+    textAlign: 'left'
+  }
+});
