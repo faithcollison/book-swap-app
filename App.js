008777d520ecb16a072c82003d9689ac4a5cd761
AnaBrh <anamariebrh@gmail.com>
2024-01-03 16:20:30 +0000

    working burger menu

--- App.js
+++ App.js
@@ -1,52 +1,86 @@
+import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import Welcome from "./components/Welcome";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import HomeScreen from "./components/Home";
+import WishList from "./components/WishList";
+import Account from "./components/Account";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Welcome"
-          component={Welcome}
-          options={{ headerTitleAlign: "center" }}
-        />
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{ headerTitleAlign: "center" }}
-        />
-        <Stack.Screen
-          name="SignUp"
-          component={SignUp}
-          options={{ headerTitleAlign: "center" }}
-        />
-        <Stack.Screen name="Home" component={HomeScreen} options={{
-          title: 'Home',
-          headerStyle: {
-            backgroundColor: '#72d5ff',
-          },
-          headerTintColor: 'black',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        }}  />
-      </Stack.Navigator>
-      <StatusBar style="auto" />
-    </NavigationContainer>
-  );
+function DrawerNavigator() {
+	return (
+		<Drawer.Navigator>
+			<Drawer.Screen
+				name="Home"
+				component={HomeScreen}
+				options={{ headerTitleAlign: "center" }}
+			/>
+			<Drawer.Screen
+				name="Account"
+				component={Account}
+				options={{ headerTitleAlign: "center" }}
+			/>
+			<Drawer.Screen
+				name="Wish List"
+				component={WishList}
+				options={{ headerTitleAlign: "center" }}
+			/>
+		</Drawer.Navigator>
+	);
 }
 
+function App() {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen
+					name="Welcome"
+					component={Welcome}
+					options={{ headerTitleAlign: "center" }}
+				/>
+				<Stack.Screen
+					name="Login"
+					component={Login}
+					options={{ headerTitleAlign: "center" }}
+				/>
+				<Stack.Screen
+					name="SignUp"
+					component={SignUp}
+					options={{ headerTitleAlign: "center" }}
+				/>
+				<Stack.Screen
+					name="Home"
+					component={DrawerNavigator}
+					options={{
+						title: "Home",
+						headerShown: false,
+						headerStyle: {
+							backgroundColor: "#72d5ff",
+						},
+						headerTintColor: "black",
+						headerTitleStyle: {
+							fontWeight: "bold",
+						},
+					}}
+				/>
+			</Stack.Navigator>
+			<StatusBar style="auto" />
+		</NavigationContainer>
+	);
+}
+
+export default App;
+
 const styles = StyleSheet.create({
-  text: {
-    alignItems: "center",
-  },
+	text: {
+		alignItems: "center",
+	},
 });
