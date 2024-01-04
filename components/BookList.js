bff554b5ce0bf4bc39dc66adcab1006bfd0ae5b8
Jake <jakehuntlyrobinson@gmail.com>
2024-01-04 23:47:29 +0000

    fix warning about list items needing keys in bookList

--- components/BookList.js
+++ components/BookList.js
@@ -33,7 +33,7 @@ export default function BookList ({ categoryName }) {
             </View>
             <ScrollView showsHorizontalScrollIndicator={false} style={styles.categoryList} horizontal={true}>
                 {bookList.map(listing => {
-                    return <Image style={styles.bookCard} source={{uri: listing.img_url}} />
+                    return <Image style={styles.bookCard} source={{uri: listing.img_url}} key={listing.book_id}/>
                 })}
             </ScrollView>
         </View>
