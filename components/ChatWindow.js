cbbe8a75b51ebf2012489ba0212c6db16a8caddd
Jake <jakehuntlyrobinson@gmail.com>
2024-01-09 18:17:32 +0000

    add some styling to the chat

--- components/ChatWindow.js
+++ components/ChatWindow.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import supabase from "../config/supabaseClient";
 
 export default function ChatWindow({ route }) {
@@ -20,17 +20,57 @@ export default function ChatWindow({ route }) {
 
   useEffect(() => {
     fetchChats().then(setChatMessages);
-  });
+  }, []);
 
   return (
-    <View>
-      {chatMessages.map((message) => {
-        return message.sender_id === session.user.id ? (
-          <Text>It's my message: {message.message}</Text>
-        ) : (
-          <Text>It's their message: {message.message}</Text>
-        );
-      })}
+    <View style={{ flex: 1 }}>
+      <ScrollView
+        style={{ position: "absolute", bottom: 0, top: 0, width: "100%" }}
+        contentContainerStyle={{ flex: 1, justifyContent: "flex-end" }}
+      >
+        {chatMessages.map((message) => {
+          if (!message) {
+            console.warn("Encountered undefined message object");
+            return null;
+          }
+          return message.sender_id === session.user.id ? (
+            <Text style={styles.senderMessage}>
+              It's my message: {message.message}
+            </Text>
+          ) : (
+            <Text style={styles.receiverMessage}>
+              It's their message: {message.message}
+            </Text>
+          );
+        })}
+      </ScrollView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    bottom: 0,
+    top: 0,
+    width: "100%",
+  },
+  senderMessage: {
+    alignSelf: "flex-end",
+    padding: 5,
+    borderColor: "gray",
+    borderWidth: 2,
+    borderRadius: 12,
+    marginTop: 5,
+    marginBottom: 5,
+  },
+  receiverMessage: {
+    alignSelf: "flex-start",
+    padding: 5,
+    borderColor: "gray",
+    borderWidth: 2,
+    borderRadius: 12,
+    marginTop: 5,
+    marginBottom: 5,
+  },
+});
