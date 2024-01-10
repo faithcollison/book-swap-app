afcc6a96b772b70834ddd81eccdc4fa4092f6066
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2024-01-10 13:17:10 +0000

    updating the notification page styling

--- components/Notifications.js
+++ components/Notifications.js
@@ -10,6 +10,10 @@ import {
 } from "react-native";
 import supabase from "../config/supabaseClient";
 import { useNavigation } from "@react-navigation/native";
+import { Entypo } from '@expo/vector-icons';
+import { Dimensions } from "react-native";
+
+const {width, height} = Dimensions.get('screen');
 
 const Notifications = ({ route }) => {
   const [notifications, setNotifications] = useState([]);
@@ -130,149 +134,139 @@ const Notifications = ({ route }) => {
   }, [notifications]);
 
   return (
-    <ScrollView style={styles.page}>
-      <View style={{ justifyContent: "flex-start" }}>
-        {processedNotifications.map((notification) => {
-          switch (notification.type) {
-            case "Swap_Request":
-              if (notification.swapData) {
-                return (
-                  <Pressable
-                    style={styles.notCard}
-                    onPress={() => {
-                      if (
-                        Object.entries(notification.swapData).every(
-                          ([key, value]) => !!value
-                        )
-                      ) {
-                        navigation.navigate("SwapNegotiationPage", {
-                          info: notification.swapData,
-                          user1_book: notification.swapData,
-                          user2_book: notification.swapData,
-                        });
-                      } else {
-                        navigation.navigate("SwapOffer", {
-                          info: notification.swapData,
-                        });
-                      }
-                    }}
-                  >
-                    <View key={notification.swapData.offer_date}>
-                      {/* <Text>{daysSince(notification.created_at)} days ago</Text> */}
-                      <View style={styles.notCard}>
-                        <Image
-                          source={{
-                            uri: notification.swapData.user1_book_imgurl,
-                          }}
-                          style={styles.bookCard}
-                        />
-                        <Text style={styles.textBox}>
-                          <Text style={styles.notParaText}>
+    <ScrollView style={styles.scroll}>
+      <View style={styles.page}>
+        <View style={styles.notificationsList}>
+          {processedNotifications.map((notification) => {
+            switch (notification.type) {
+              case "Swap_Request":
+                if (notification.swapData) {
+                  return (
+                    <Pressable
+                      style={styles.notCard}
+                      onPress={() => {
+                        if (
+                          Object.entries(notification.swapData).every(
+                            ([key, value]) => !!value
+                          )
+                        ) {
+                          navigation.navigate("SwapNegotiationPage", {
+                            info: notification.swapData,
+                            user1_book: notification.swapData,
+                            user2_book: notification.swapData,
+                          });
+                        } else {
+                          navigation.navigate("SwapOffer", {
+                            info: notification.swapData,
+                          });
+                        }
+                      }}
+                    >
+                        {/* <Text>{daysSince(notification.created_at)} days ago</Text> */}
+                        <View key={notification.swapData.offer_date} style={styles.contentsMain}>
+                          <Image
+                            source={{
+                              uri: notification.swapData.user1_book_imgurl,
+                            }}
+                            style={styles.bookImage}
+                          />
+                          <Text style={styles.notText}>
                             {notification.swapData.user2_username + " wants to swap " + notification.swapData.user1_book_title}
                           </Text>
+                        </View>
+                        <Entypo name="circle-with-cross" size={24} color='#C2C2C2' onPress={() => deleteNotification(notification.id)} style={styles.deleteButton}/>
+                    </Pressable>
+                  );
+                }
+                break;
+              case "Chosen_Book":
+                if (notification) {
+                  return (
+                    <Pressable
+                      style={styles.container}
+                      onPress={() => {
+                        navigation.navigate("SwapNegotiationPage", {
+                          info: notification,
+                        });
+                      }}
+                    >
+                      <View>
+                        <Text>{daysSince(notification.created_at)} days ago</Text>
+                        <Text style={styles.textBox}>
+                          <Text style={styles.notParaText}>
+                            {notification.username}{" "}
+                          </Text>
+                          <Image
+                            source={{ uri: notification.swapData }}
+                            class={styles.image}
+                          />
+                          <Text>has chosen a book from your library</Text>
+                          <Pressable
+                            onPress={() => deleteNotification(notification.id)}
+                            style={styles.deleteButton}
+                          >
+                            <Text style={{ color: "red" }}>Delete</Text>
+                          </Pressable>
                         </Text>
                       </View>
-                      
-                      
-                      
-                      
-                      {/* <Pressable
-                        onPress={() => deleteNotification(notification.id)}
-                        style={styles.deleteButton}
-                      >
-                        <Text style={{ color: "red" }}>Delete</Text>
-                      </Pressable> */}
-
-
-
-                    </View>
-                  </Pressable>
-                );
-              }
-              break;
-            case "Chosen_Book":
-              if (notification) {
-                return (
-                  <Pressable
-                    style={styles.container}
-                    onPress={() => {
-                      navigation.navigate("SwapNegotiationPage", {
-                        info: notification,
-                      });
-                    }}
-                  >
-                    <View>
-                      <Text>{daysSince(notification.created_at)} days ago</Text>
-                      <Text style={styles.textBox}>
-                        <Text style={styles.notParaText}>
-                          {notification.username}{" "}
-                        </Text>
-                        <Image
-                          source={{ uri: notification.swapData }}
-                          class={styles.image}
-                        />
-                        <Text>has chosen a book from your library</Text>
-                        <Pressable
-                          onPress={() => deleteNotification(notification.id)}
-                          style={styles.deleteButton}
-                        >
-                          <Text style={{ color: "red" }}>Delete</Text>
-                        </Pressable>
-                      </Text>
-                    </View>
-                  </Pressable>
-                );
-              }
-              break;
-            default:
-              return null;
-          }
-        })}
+                    </Pressable>
+                  );
+                }
+                break;
+              default:
+                return null;
+            }
+          })}
+        </View>
       </View>
     </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  page: {
+  scroll: {
     backgroundColor: "#272727",
+  },
+  page: {
     flex: 1,
+    alignItems: 'center',
   },
-  notParaText: {
-    // fontWeight: "bold",
-    color: "white",
-    flex: 0.5,
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-  },
-  notText: {
-    color: "white",
-  },
-  bookCard: {
-    // flex: 0.5,
-    borderRadius: 16,
-    height: 180,
-    width: 120,
-    resizeMode: "contain",
+  notificationsList: {
+    width: width * 0.9,
   },
   notCard: {
-    flex: 1,
     backgroundColor:"#464646",
-    width: "auto",
-    alignItems: "center",
-    justifyContent: "center",
-    width: "90%",
-    // borderRadius: 12,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginTop: 20,
+    padding: 10,
+  },
+  contentsMain: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: width * 0.9 - 20,
+    height: 90,
+  },
+  bookImage: {
+    height: 90,
+    width: 60,
+    borderRadius: 6,
+    // marginRight: 20,
+
+  },
+  notText: {
+    color: '#C2C2C2',
+    width: width * 0.9 * 0.9 * 0.8,
   },
   deleteButton: {
-    marginTop: 8,
-    alignItems: "center",
-  },
-  textBox: {
-    display: "flex",
-    // alignItems: "flex-start",
-    padding: 5,
-  },
+    position: 'absolute',
+    right: 8,
+    top: 7,
+  }
 });
 
 export default Notifications;
